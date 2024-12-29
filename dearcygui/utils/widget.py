c99d0e82ef841d1360e8de8fe0583e95d3689ebe
Axel Davy <davyaxel0@gmail.com>
2024-12-29 23:00:02 +0100

    utils: Use new handler features to simplify TemporaryTooltip

--- dearcygui/utils/widget.py
+++ dearcygui/utils/widget.py
@@ -14,26 +14,12 @@ class TemporaryTooltip(dcg.Tooltip):
                  context : dcg.Context,
                  **kwargs):
         super().__init__(context, **kwargs)
-        not_rendered = dcg.OtherItemHandler(context, target=self, op=dcg.HandlerListOP.NONE, callback=self.destroy_tooltip)
-        with not_rendered:
-            dcg.RenderHandler(context)
-        self.viewport_handler = not_rendered
-        # += is not atomic. The mutex is to be thread safe, in case
-        # another thread manipulates the handlers
-        with context.viewport.mutex:
-            context.viewport.handlers += [self.viewport_handler]
-
-    def cleanup_handlers(self):
-        # Remove the handlers we attached
-        with self.context.viewport.mutex:
-            self.context.viewport.handlers = [
-                h for h in self.context.viewport.handlers\
-                if h is not self.viewport_handler
-            ]
+        self.handlers += [
+            dcg.LostRenderHandler(context,
+                                  callback=self.destroy_tooltip)]
 
     def destroy_tooltip(self):
         if self.context is None:
             return # Already deleted
-        self.cleanup_handlers()
         # self.parent = None would work too but would wait GC.
         self.delete_item()
